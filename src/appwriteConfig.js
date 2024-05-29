97c3b7fe974666b0752fa97ab93ab435968faf3d
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-05-29 14:44:11 +0530

    some changes

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -3,7 +3,7 @@ import { Account, Client, Databases } from "appwrite";
 export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
 export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID
-export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_COLLECTION_ID_MESSAGES
+export const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID_MESSAGES
 
 const client = new Client();
 
